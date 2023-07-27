615dd28b821b307855c386d11a77303b2a13bd04
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-07-27 15:09:06 +0530

    displays the point of equality

--- pivotIndex.cpp
+++ pivotIndex.cpp
@@ -5,30 +5,29 @@
 using namespace std;
 int main() {
     // Write C++ code here
-    vector <int> nums = {1,7,3,6,5,6};
+    vector <int> nums = {2,1,-1};
     int pivot;
     int len = nums.size();
     int ptr =1;
     int ls = 0;
     int rs = 0;
+    int flag = 0;
     // cout<<accumulate(nums.begin(),nums.begin()+1, 0);
     // cout<<accumulate(nums.begin()+2,nums.end(),0);
     // cout<<ls;
     for(int i = 0; i<len;i++){
-        // if(i==0){
-        //     ls = 0;
-            
-        // }
         ls = accumulate(nums.begin(),nums.begin()+i, 0);
         cout<<i<<"\t";
         cout<<ls<<"\t";
         rs = accumulate(nums.begin()+i+1,nums.end(),0);
         cout<<rs<<"\n";
-        // if(ls==rs){
-        //     ptr = i;
-        //     break;
-        // }
+        if(ls==rs){
+            flag = 1;
+            ptr = i;
+            break;
+        }
     }
-    cout<<ptr;
-    // return 0;
+    if(!flag) cout<<"nt";
+    else cout<<ptr;
+    return 0;
 }
